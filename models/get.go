ed88d403f30fa87f231bedade6703678e5412ba0
Arthur Brasa <arthurbrasa@outlook.com>
2023-06-20 23:15:52 -0300

    Get

---
+++ models/get.go
@@ -0,0 +1,18 @@
+package models
+
+import "github.com/ArthurBrasa/API-GO/db"
+
+func get(id int64) (todo Todo, err error) {
+	connection, err := db.OpenConnection()
+	if err != nil {
+		return
+	}
+
+	defer connection.Close()
+
+	row := connection.QueryRow(`SELECT * FROM todos WHERE id = $1`, id)
+
+	err = row.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+
+	return
+}
